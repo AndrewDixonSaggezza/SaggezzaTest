f28a952fc22bc2b3d71a921124d03aa4dfb06226
Andy <Andrew.dixon@saggezza.com>
2018-10-29 10:25:57 +0000

    Default ASP.NET MVC set-up

---
+++ SaggezzaTest/Controllers/EmployeesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SaggezzaTest.Models;
+
+namespace SaggezzaTest.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+            var employee = new Employee() { Name = "RandomEmployee" };
+            return View(employee);
+        }
+    }
+}
