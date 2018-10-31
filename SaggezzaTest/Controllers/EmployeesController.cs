e7d52d23e375b1bf86c081231bbb465854c87f23
Andy <Andrew.dixon@saggezza.com>
2018-10-31 21:55:15 +0000

    Updates

--- SaggezzaTest/Controllers/EmployeesController.cs
+++ SaggezzaTest/Controllers/EmployeesController.cs
@@ -9,11 +9,20 @@ namespace SaggezzaTest.Controllers
 {
     public class EmployeesController : Controller
     {
+        [Route("employees/skills/{skill}/{capability:range(1-5)}")]
         // GET: Employees
-        public ActionResult Index()
+        public ActionResult Index(int? pageIndex, string sortBy)
         {
-            var employee = new Employee() { Name = "RandomEmployee" };
-            return View(employee);
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult BySkill(string skill, int capability)
+        {
+            return Content(skill + " : " + capability);
         }
     }
 }
